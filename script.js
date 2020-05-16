f4edc436677942d9a758d89f923b259990310aa8
riddhitanna <62380996+riddhitanna@users.noreply.github.com>
2020-05-17 01:17:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function login(){
+  $("#container").fadeIn("2");
+  $("#login-icon").fadeOut();
+  $(".close-btn").fadeIn("2");
+  $("html").css("background","opacity:0.5");
+}
+
+function closed(){
+  $("#container").fadeOut();
+  $("#fcontainer").css("display","none");
+  $("#login-icon").fadeIn();
+  
+}
+
+function clicked(){
+
+var emppass = document.forms["f1"]["password"].value;
+$("#container").fadeOut();
+alert("WELCOME!");
+}
+
+
+function forgotten(){
+  $("#container").fadeOut(function(){
+    $("#fcontainer").css("display","initial");
+  });
+}
+
+function Pass(){
+  alert("Please check your inbox!");
+
+}
+
+  
+
+
